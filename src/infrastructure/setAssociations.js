58c8f6d3fbbfa9aa6620d51a062f816fc4e337ff
Etienne Ludot <MY_NAME@example.comSystem.out.println(ClickMenu = loginRepository.getUser().getRole());larave>
2021-02-09 17:09:49 +0100

    add Role

--- src/infrastructure/setAssociations.js
+++ src/infrastructure/setAssociations.js
@@ -1,13 +1,16 @@
 module.exports = (sequelize) => {
     const {user, user_read_chapter, chapter, lesson, comments, role} = sequelize.models;
 
-    lesson.hasMany(user);
-
     user_read_chapter.belongsTo(user);
     
-    chapter.belongsTo(user_read_chapter);
+    user_read_chapter.belongsTo(chapter);
 
     //Create foreign key for the Comments model
     comments.belongsTo(chapter);
     comments.belongsTo(user);
+
+    lesson.belongsTo(user);
+    chapter.belongsTo(lesson);
+
+    role.belongsTo(user);
 }
