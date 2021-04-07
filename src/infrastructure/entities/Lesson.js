712c6916891808cb7db98fd3248c285cfb316f5f
etienne <etienne@localhost.localdomain>
2021-04-07 14:20:07 +0200

    add false to reputation

--- src/infrastructure/entities/Lesson.js
+++ src/infrastructure/entities/Lesson.js
@@ -25,7 +25,7 @@ module.exports = (sequelize) => {
         },
         reputation: {
             type: DataTypes.INTEGER,
-            allowNull: true
+            allowNull: false
         }
     }, {
         timestamps: false,
